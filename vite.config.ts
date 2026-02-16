f36dfdafdd8d7009f1f2546f18406373d283e099
Baronman <failingitup@gmail.com>
2026-02-16 07:38:46 -0500

    Web Test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = "AppointmentsClientSide";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/" // <-- use "/" for localhost
+  base: `/${repoName}/`,
 });
